d73f439f4e05841f99a24787c5e4818acfa0098b
komaku-program <t.hirabayashi0430@gmail.com>
2023-08-21 11:42:31 +0900

    create app foundation

---
+++ src/pages/edit-post/[id].tsx
@@ -0,0 +1,70 @@
+import { Post } from "@/types";
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, { ChangeEvent, FormEvent, use, useState } from "react";
+
+type Props = {
+  post: Post;
+};
+
+export async function getServerSideProps(context: any) {
+  const id = context.params.id;
+
+  const res = await fetch(`http://localhost:3001/api/v1/posts/${id}`);
+  const post = await res.json();
+
+  return {
+    props: {
+      post,
+    },
+  };
+}
+
+const EditPost = ({ post }: Props) => {
+  // 後日UseFormなるものを追加予定
+  const [title, setTitle] = useState(post.title);
+  const [content, setContent] = useState(post.content);
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await axios.put(`http://localhost:3001/api/v1/posts/${post.id}`, {
+        title: title,
+        content: content,
+      });
+
+      router.push("/");
+    } catch (err) {
+      alert("編集に失敗しました");
+    }
+  };
+
+  return (
+    <div>
+      <h1>ブログ編集</h1>
+      <form onSubmit={handleSubmit}>
+        <label>タイトル</label>
+        <input
+          type="text"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setTitle(e.target.value)
+          }
+          value={title}
+        />
+        <label>本文</label>
+        <textarea
+          onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+            setContent(e.target.value)
+          }
+          value={content}
+        />
+
+        <button type="submit">編集</button>
+      </form>
+    </div>
+  );
+};
+
+export default EditPost;
---
+++ src/pages/posts/[id].tsx
@@ -0,0 +1,52 @@
+import { Post } from "@/types";
+import { useRouter } from "next/router";
+import { type } from "os";
+import React from "react";
+
+type Props = {
+  post: Post;
+};
+
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:3001/api/v1/posts");
+  const posts: Post[] = await res.json();
+
+  const paths = posts.map((post) => ({
+    params: { id: post.id.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }: { params: { id: string } }) {
+  const res = await fetch(`http://localhost:3001/api/v1/posts/${params.id}`);
+  const post = await res.json();
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60,
+  };
+}
+
+const Post = ({ post }: Props) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <div>{post.title}</div>
+      <div>{post.created_at}</div>
+      <p>{post.content}</p>
+    </div>
+  );
+};
+
+export default Post;
