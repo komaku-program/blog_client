d78f1bb2ec39b81b8e7f9399d63b02640414b081
komaku-program <t.hirabayashi0430@gmail.com>
2023-09-06 22:20:07 +0900

    edit signup form

--- src/components/SignupModal/index.tsx
+++ src/components/SignupModal/index.tsx
@@ -55,20 +55,9 @@ const SignupModal: React.FC<LoginModalProps> = ({
         <h1>ようこそ</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div>
-            <label>ユーザー名</label>
-            <input
-              autoComplete="off"
-              type="text"
-              {...register("name", {
-                required: "ユーザー名は必須です",
-              })}
-            />
-            {errors.email && <p>{errors.email.message}</p>}
-          </div>
-
-          <div>
             <label>Email</label>
             <input
+              autoComplete="off"
               type="text"
               {...register("email", {
                 required: "Emailは必須です",
@@ -92,12 +81,24 @@ const SignupModal: React.FC<LoginModalProps> = ({
           <div>
             <label>PW(再入力)</label>
             <input
+              autoComplete="off"
               type="password"
               {...register("password_confirmation", {
                 required: "パスワードの確認は必須です",
               })}
             />
             {errors.password && <p>{errors.password.message}</p>}
+          </div>
+          <div>
+            <label>ユーザー名</label>
+            <input
+              autoComplete="off"
+              type="text"
+              {...register("name", {
+                required: "ユーザー名は必須です",
+              })}
+            />
+            {errors.email && <p>{errors.email.message}</p>}
           </div>
 
           <button type="submit">サインアップ</button>
